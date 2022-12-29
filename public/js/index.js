23d9578311ffaa926bd49ba165e86802fa709926
micheci <michecimartinez@gmail.com>
2022-12-29 10:40:36 -0600

    added footer n yo==updated toggle

--- public/js/index.js
+++ public/js/index.js
@@ -6,19 +6,15 @@ const submit = document.getElementById('submit');
 submit.addEventListener('submit',(e)=>{
     e.preventDefault();
     
-    let ebody = `
-    <h1>First Name: </h1>${fname.value}
-    <br>
-    <h1>Last Name: </h1>${lname.value}
-    `;
+   
 
     Email.send({
         SecureToken : "9fe06437-9e83-42d2-b388-3a9494507834", //add your token here
-        To : "sniperpig0@gmail.com", 
+        To :   `${toEmail.value}`, 
         From : "sniperpig0@gmail.com",
         Subject : "Thanks for signing up to this culture",
         Body : "HEY this test email sender works :) and thx for siging up for the newslater, jk unless..."
     }).then(
-      message => alert("YO")
+      message => alert("Sent!")
     );
 }); 
--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,4 +1,5 @@
 function Toggle() {
     var element = document.body;
     element.classList.toggle("lightMode");
+    btn.textContent = 'Button clicked';
   }
